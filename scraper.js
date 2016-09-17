5056f8ccfb4daf70453a1125d67ebfe86627f1b9
sudiptokarmoker <sudiptocsi@gmail.com>
2016-09-17 13:06:51 +0600

    Update scraper.js
    Set status code

--- scraper.js
+++ scraper.js
@@ -17,26 +17,26 @@ var service = server.listen(ip_server, function(request, response) {
   var url_to_scrap = str.split("/?url=");
   
 //console
-
+/*
 casper.start(url_to_scrap[1], function() {
     //this.echo(this.getHTML());
 });
+*/
+  casper.start(url_to_scrap[1]);
 
   casper.then(function() {
     // aggregate results for the 'casperjs' search
-    //links = this.evaluate(getLinks);
-	
-	content_page = this.getHTML();
-	
+    //links = this.evaluate(getLinks); 
+	content_page = this.getHTML(); 
   });
 
-casper.on("page.error", function(msg, trace) {
+  casper.on("page.error", function(msg, trace) {
     response.write("fail");
     response.close();
 });
 
   casper.run(function() {
-    //response.statusCode = 200;
+    response.statusCode = 200;
 
     //sends results as JSON object
     response.write(content_page);
